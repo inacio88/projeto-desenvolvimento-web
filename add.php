2a6f09c0ed6a00a3233fdb540e56226797f784e3
inacio88 <inacio88@outlook.com>
2022-04-24 16:37:22 -0300

    conexão com o banco

--- add.php
+++ add.php
@@ -1,6 +1,7 @@
 <?php
+    $email = $nomePedido = $adicionais = '';
 
-   
+   $erros = array('email'=>'','nomePedido'=>'', 'adicionais'=>'');
 
     if (isset($_POST['submit'])) {
         //Significa que algo foi enviado
@@ -12,43 +13,54 @@
         //Fazendo validação server-side no php, mas poderia também ter sido feita no html
         if (empty($_POST['email'])) {
             //Fazer algo em js aqui
-            echo 'Preencha um email! <br/>';
+            $erros['email'] = 'Preencha um email! <br/>';
         }
         else{
 
             //echo htmlspecialchars($_POST['email']);
             $email = $_POST['email'];
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo 'Esse não é um email válido <br/>';
+                $erros['email'] = 'Esse não é um email válido <br/>';
             }
         }
 
         //---------------
         if (empty($_POST['nomePedido'])) {
             //Fazer algo em js aqui
-            echo 'Preencha um nome Pedido! <br/>';
+            $erros['nomePedido'] = 'Preencha um nome Pedido! <br/>';
         }
         else{
             //echo htmlspecialchars($_POST['nomePedido']);
             //Usando regex para ver se o pedido é valido
             $nomePedido = $_POST['nomePedido'];
             if (!preg_match('/^[a-zA-Z\s]+$/', $nomePedido)) {
-                echo ('O nome do pedido está inválido <br/>');
+                $erros['nomePedido'] = 'O nome do pedido está inválido <br/>';
             }
         }
 
         //-----------------
         if (empty($_POST['adicionais'])) {
             //Fazer algo em js aqui
-            echo 'Preencha  adicionais! <br/>';
+            $erros['adicionais'] = 'Preencha  adicionais! <br/>';
         }
         else{
             //echo htmlspecialchars($_POST['adicionais']);
             $adicionais = $_POST['adicionais'];
             if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $adicionais)) {
-                echo ('O campo de adicionais está inválido <br/>');
+                $erros['adicionais'] =  'O campo de adicionais está inválido <br/>';
             }
         }
+
+        //Se todas as posições desse array estirem vazias, então essa função retorna falso
+        //No caso, se tiver alguma msg nesse array é pq teve erro, e a função retorna true
+        if (array_filter($erros)) {
+            //echo 'Tem erro no formulário';
+        }
+        else{
+            //echo 'O formulário está válido';
+            header('Location: index.php');
+        }
+
 
     }// Fim da verificação do POST
 
@@ -64,11 +76,17 @@
         <h4 class="center">Add pedido</h4>
         <form action="add.php" method="POST" class="white">
             <label> Email: </label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <div class="red-text"> <?php echo $erros['email']; ?> </div>
+
             <label> Nome do pedido: </label>
-            <input type="text" name="nomePedido">
+            <input type="text" name="nomePedido" value="<?php echo htmlspecialchars($nomePedido) ?>"> 
+            <div class="red-text"> <?php echo $erros['nomePedido']; ?> </div>
+
             <label> Adicionais: </label>
-            <input type="text" name="adicionais">
+            <input type="text" name="adicionais" value="<?php echo htmlspecialchars($adicionais) ?>">
+            <div class="red-text"> <?php echo $erros['adicionais']; ?> </div>
+
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand">
             </div>
--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
+// Duas maneira de conectar com o mysql
+// MySQLi (i significa improved) e PDO (php data object)
+// MySQLi
 
+$conn = mysqli_connect('localhost', 'db_user', '1234', 'restaurante_db');
+
+//Checar a conexão
+if (!$conn) {
+    echo 'erro na conexão mysql' . mysqli_connect_error();
+}
+
+// query para os pedidos
+$sql = 'SELECT nomePedido, adicionais, id FROM pedidos ORDER BY created_at';
+
+// fazer a query e pegar os retornos
+$result = mysqli_query($conn, $sql);
+
+//Pegar o resultado como linhas em um array
+$pedidos = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//Liberar o resultado da memória
+
+mysqli_free_result($result);
+
+//Fechar a conexão com o banco
+mysqli_close($conn);
+
+//Imprimindo o array
+//print_r($pedidos);
+
+
+//
+//print_r(explode(',', $pedidos[0]['adicionais']));
 
 ?>
 
@@ -7,6 +39,30 @@
 <html lang="pt-br">
 
     <?php include('templates/header.php'); ?>
+
+    <h4 class="center grey-text">Pedidos</h4>
+    <div class="container">
+        <div class="row">
+            <?php foreach($pedidos as $pedido){ ?>
+                <div class="col s6 md3">
+                    <div class="card">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pedido['nomePedido']);?></h6>
+                            <ul>
+                                <?php foreach (explode(',', $pedido['adicionais']) as $ing ) { ?>
+                                    <li> <?php echo htmlspecialchars($ing); ?> </li>
+                                <?php } ?>
+                            </ul>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">Mais informações</a>
+                        </div>
+                    </div>
+                </div>
+            <?php }?>
+        </div>
+    </div>
+
     <?php include('templates/footer.php'); ?>
     
 
