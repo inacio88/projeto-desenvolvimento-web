b590c6029e43aee04d41d6d3bb8c0dc2150046b8
inacio88 <inacio88@outlook.com>
2022-04-24 20:10:08 -0300

    add img

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ mysqli_close($conn);
             <?php foreach($pedidos as $pedido): ?>
                 <div class="col s6 md3">
                     <div class="card">
+                        <img src="img/pizza.svg" class="pizza">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pedido['nomePedido']);?></h6>
                             <ul>
